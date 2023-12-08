bcbef9ce26492f34f91fa5d5af0c4efa651ef82a
Avinashfizz <130842159+Avinashfizz@users.noreply.github.com>
2023-12-08 16:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const extendHex = (shortHex) => {
   // write your code here
 
-  let i = shortHex.length()-3;
-  let isCapital = false;
+  var i = shortHex.length()-3;
+  var isCapital = false;
 	for(let j=0; j<shortHex.length()){
 		if(shortHex[j]>=65 && shortHex[j]<=90)isCapital=true;
 		j++;
 	}
-  let ans="#"+"shortHex[i]"+"shortHex[i]"+"shortHex[i+1]"+"shortHex[i+1]"+"shortHex[i+2]"+"shortHex[i+2]";
+  var ans="#"+"shortHex[i]"+"shortHex[i]"+"shortHex[i+1]"+"shortHex[i+1]"+"shortHex[i+2]"+"shortHex[i+2]";
 	if(isCapital===true)ans=ans.toUpperCase();
 	return ans;
 };
